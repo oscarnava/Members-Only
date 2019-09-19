7419789f3d6e4d7d7ce43a8b732c81ef2a345af2
Mauricio Robayo <rfmajo@gmail.com>
2019-09-19 13:00:26 -0500

    Render flash on layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+
     @user = User.find_by(email: params[:session][:email])
-    if @user.authenticate(params[:session][:password])
+
+    if @user&.authenticate(params[:session][:password])
+      new_token = User.new_token
+      cookies.permanent[:remember_token] = new_token
       session[:remember_token] = @user.remember_token
       flash[:notice] = 'Eureka!'
       redirect_to :root
     else
+      flash.now[:notice] = 'Wrong password or username!'
       render :create
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ class User < ApplicationRecord
   before_create :create_token
   has_secure_password
 
+  def self.new_token
+    SecureRandom.urlsafe_base64.to_s
+  end
+
   private
 
   def create_token
-    self.remember_token = Digest::SHA1.hexdigest(SecureRandom.urlsafe_base64.to_s)
+    self.remember_token = Digest::SHA1.hexdigest(new_token)
   end
 end
