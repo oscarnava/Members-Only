f192fdc2a88660d75f64f93676511ee452541774
Mauricio Robayo <rfmajo@gmail.com>
2019-09-19 11:31:31 -0500

    Setup user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,5 @@
+class SessionsController < ApplicationController
+  def new
+    @session = nil
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  before_action :create_token
   has_secure_password
+
+  private
+
+  def create_token
+
+  end
 end
